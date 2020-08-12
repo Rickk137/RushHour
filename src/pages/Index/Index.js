7100e8897848ef2fc8db4b8da8c56b3a5a3ab123
peymannaderi3 <peymannaderi3@yahoo.com>
2020-08-12 04:39:40 +0430

    add: index page.

--- src/pages/Index/Index.js
+++ src/pages/Index/Index.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+//componnets
+import Board from '../../components/pages/Index/Board/Board';
+import Timer from '../../components/pages/Index/Timer/Timer';
+import Help from '../../components/pages/Index/Help/Help';
 //styles
-// import classes from './Index.module.scss';
+import classes from './Index.module.scss';
 
-export const Index = () => {
+const Index = () => {
+  const [freeze, setFreeze] = useState(false);
+
   return (
-    <>
-      <h3>Index</h3>
-    </>
+    <div className={classes.container}>
+      <Timer setFreeze={setFreeze} freeze={freeze} />
+      <Board setFreeze={setFreeze} freeze={freeze} />
+      <Help />
+    </div>
   );
 };
+
+export default Index;
