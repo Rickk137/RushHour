8919d5ddd933aeb08ded73be8baae1c6b827b3a7
peymannaderi3 <peymannaderi3@yahoo.com>
2020-08-12 15:13:22 +0430

    fix: pwa.

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,7 @@
 const CACHE_NAME = 'CarCache';
 
 self.addEventListener('fetch', function (event) {
+  if (!(event.request.url.indexOf('http') === 0)) return;
   event.respondWith(
     caches.open(CACHE_NAME).then(function (cache) {
       return cache.match(event.request).then(function (response) {
