8e93a077b98b3d9ddda341b1b268c9f39d30d48f
peymannaderi3 <peymannaderi3@yahoo.com>
2020-08-12 04:38:55 +0430

    add: help component.

---
+++ src/components/pages/Index/Help/Help.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Help = ({}) => {
+  return (
+    <div>
+      <p>Select a car and move it with arrow keys.</p>
+      <p>
+        The game will be finished whenever the user exits the red car out of the
+        map
+      </p>
+    </div>
+  );
+};
+
+export default Help;
